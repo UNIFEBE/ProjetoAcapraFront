89030fc675fef193da8a98f386e5c39525025dd1
LuisFelipeBarnabe <luisrbarnabe@gmail.com>
2025-05-14 19:21:33 -0300

    add Accordions

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -1,23 +1,48 @@
 import Navbar from "../componentes/Navbar/Navbar";
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import {ChevronDown} from 'mdi-material-ui'; 
+
+
 
 export const FAQ = () => {
     return (
-        <div style={{ paddingTop: '70px' }}>
+        <div style={{ paddingTop: '70px', marginTop: '20px'}}>
             <Navbar />
-            <div className="faq-container">
-                <h1>Perguntas Frequentes</h1>
-                <div className="faq-item">
-                    <h2>Como posso adotar um animal?</h2>
-                    <p>Para adotar um animal, você pode visitar nossa seção de adoção e preencher o formulário de interesse.</p>
-                </div>
-                <div className="faq-item">
-                    <h2>Quais são os requisitos para adoção?</h2>
-                    <p>Os requisitos incluem ter mais de 18 anos, fornecer um comprovante de residência e estar disposto a cuidar do animal.</p>
-                </div>
-                <div className="faq-item">
-                    <h2>Vocês oferecem suporte pós-adoção?</h2>
-                    <p>Sim, oferecemos suporte pós-adoção para ajudar com qualquer dúvida ou problema que você possa ter.</p>
-                </div>
+            <div style={{paddingInline: '10%', marginTop: '20px'}}>
+        <Accordion style={{marginTop: '10px'}}>
+        <AccordionSummary
+          expandIcon={<ChevronDown />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          <Typography component="span"><u>Como posso adotar um animal?</u></Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Para adotar um animal, você pode visitar nossa seção de adoção e preencher o formulário de interesse.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion style={{marginTop: '10px'}}>
+        <AccordionSummary
+          expandIcon={<ChevronDown />}
+          aria-controls="panel2-content"
+          id="panel2-header"
+        >
+          <Typography component="span"><u>Quais são os requisitos para adoção?</u></Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Sim, oferecemos suporte pós-adoção para ajudar com qualquer dúvida ou problema que você possa ter.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
             </div>
         </div>
     )
