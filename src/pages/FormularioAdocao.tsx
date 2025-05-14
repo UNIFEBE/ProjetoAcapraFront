ef06a71d68c199b650f5abd1777714eae79f9bf7
LuisFelipePeirao <luis.peirao@unifebe.edu.br>
2025-05-14 17:42:14 -0300

    Criação da tela de formulário de adoção (ajuste da tag <form>)

--- src/pages/FormularioAdocao.tsx
+++ src/pages/FormularioAdocao.tsx
@@ -65,158 +65,160 @@ export default function FormularioAdocao() {
                         Formulário de Adoção
                     </Typography>
 
-                    <Grid
-                        container
-                        spacing={2}
-                        justifyContent="space-between"
-                        alignItems="center"
-                        sx={{ mb: 2 }}
-                    >
+                    <form onSubmit={handleSubmit}>
+                        <Grid
+                            container
+                            spacing={2}
+                            justifyContent="space-between"
+                            alignItems="center"
+                            sx={{ mb: 2 }}
+                        >
 
-                        <Grid>
-                            <Grid
-                                container
-                                spacing={2}
-                                justifyContent="space-between"
-                                alignItems="center"
-                            >
-                                <Grid sx={{ p: 2 }}>
-                                    <TextField
-                                        fullWidth
-                                        label="Nome Completo"
-                                        name="nome"
-                                        value={form.nome}
-                                        onChange={handleChange}
-                                        variant="outlined"
-                                        required
-                                    />
-                                </Grid>
-                                <Grid sx={{ p: 2 }}>
-                                    <TextField
-                                        fullWidth
-                                        label="CPF"
-                                        name="cpf"
-                                        value={form.cpf}
-                                        onChange={handleChange}
-                                        variant="outlined"
-                                        required
-                                    />
-                                </Grid>
-                            </Grid>
-                            <Grid
-                                container
-                                spacing={2}
-                                justifyContent="space-between"
-                                alignItems="center"
-                            >
-                                <Grid sx={{ p: 2 }}>
-                                    <TextField
-                                        fullWidth
-                                        label="E-mail"
-                                        name="email"
-                                        value={form.email}
-                                        onChange={handleChange}
-                                        variant="outlined"
-                                        required
-                                    />
-                                </Grid>
-                                <Grid sx={{ p: 2 }}>
-                                    <TextField
-                                        fullWidth
-                                        label="Telefone"
-                                        name="telefone"
-                                        value={form.telefone}
-                                        onChange={handleChange}
-                                        variant="outlined"
-                                        required
-                                    />
-                                </Grid>
-                            </Grid>
-                            <Grid
-                                container
-                                spacing={2}
-                                justifyContent="space-between"
-                                alignItems="center"
-                            >
-                                <Grid sx={{ p: 2 }}>
-                                    <TextField
-                                        fullWidth
-                                        label="CEP"
-                                        name="cep"
-                                        value={form.cep}
-                                        onChange={handleChange}
-                                        variant="outlined"
-                                    />
-                                </Grid>
-                                <Grid sx={{ p: 2 }}>
-                                    <TextField
-                                        fullWidth
-                                        label="Tipo de Residência"
-                                        name="tipoResidencia"
-                                        value={form.tipoResidencia}
-                                        onChange={handleChange}
-                                        variant="outlined"
-                                    />
-                                </Grid>
-                            </Grid>
-                            <Grid
-                                container
-                                spacing={2}
-                                justifyContent="space-between"
-                                alignItems="center"
-                            >
-                                <Grid sx={{ p: 2 }}>
-                                    <TextField
-                                        fullWidth
-                                        label="Data de Nascimento"
-                                        name="dataNascimento"
-                                        value={form.dataNascimento}
-                                        onChange={handleChange}
-                                        variant="outlined"
-                                    />
-                                </Grid>
-                                <Grid sx={{ p: 2 }}>
-                                    <TextField
-                                        fullWidth
-                                        label="Sexo"
-                                        name="sexo"
-                                        value={form.sexo}
-                                        onChange={handleChange}
-                                        variant="outlined"
-                                    />
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                        <Grid>
                             <Grid>
-                                <Box display="flex" justifyContent="center" alignItems="center" height="100%">
-                                    <img
-                                        src="https://images.dog.ceo/breeds/retriever-golden/n02099601_3007.jpg"
-                                        alt="Pet para adoção"
-                                        style={{ width: '100%', borderRadius: 8 }}
-                                    />
-                                </Box>
+                                <Grid
+                                    container
+                                    spacing={2}
+                                    justifyContent="space-between"
+                                    alignItems="center"
+                                >
+                                    <Grid sx={{ p: 2 }}>
+                                        <TextField
+                                            fullWidth
+                                            label="Nome Completo"
+                                            name="nome"
+                                            value={form.nome}
+                                            onChange={handleChange}
+                                            variant="outlined"
+                                            required
+                                        />
+                                    </Grid>
+                                    <Grid sx={{ p: 2 }}>
+                                        <TextField
+                                            fullWidth
+                                            label="CPF"
+                                            name="cpf"
+                                            value={form.cpf}
+                                            onChange={handleChange}
+                                            variant="outlined"
+                                            required
+                                        />
+                                    </Grid>
+                                </Grid>
+                                <Grid
+                                    container
+                                    spacing={2}
+                                    justifyContent="space-between"
+                                    alignItems="center"
+                                >
+                                    <Grid sx={{ p: 2 }}>
+                                        <TextField
+                                            fullWidth
+                                            label="E-mail"
+                                            name="email"
+                                            value={form.email}
+                                            onChange={handleChange}
+                                            variant="outlined"
+                                            required
+                                        />
+                                    </Grid>
+                                    <Grid sx={{ p: 2 }}>
+                                        <TextField
+                                            fullWidth
+                                            label="Telefone"
+                                            name="telefone"
+                                            value={form.telefone}
+                                            onChange={handleChange}
+                                            variant="outlined"
+                                            required
+                                        />
+                                    </Grid>
+                                </Grid>
+                                <Grid
+                                    container
+                                    spacing={2}
+                                    justifyContent="space-between"
+                                    alignItems="center"
+                                >
+                                    <Grid sx={{ p: 2 }}>
+                                        <TextField
+                                            fullWidth
+                                            label="CEP"
+                                            name="cep"
+                                            value={form.cep}
+                                            onChange={handleChange}
+                                            variant="outlined"
+                                        />
+                                    </Grid>
+                                    <Grid sx={{ p: 2 }}>
+                                        <TextField
+                                            fullWidth
+                                            label="Tipo de Residência"
+                                            name="tipoResidencia"
+                                            value={form.tipoResidencia}
+                                            onChange={handleChange}
+                                            variant="outlined"
+                                        />
+                                    </Grid>
+                                </Grid>
+                                <Grid
+                                    container
+                                    spacing={2}
+                                    justifyContent="space-between"
+                                    alignItems="center"
+                                >
+                                    <Grid sx={{ p: 2 }}>
+                                        <TextField
+                                            fullWidth
+                                            label="Data de Nascimento"
+                                            name="dataNascimento"
+                                            value={form.dataNascimento}
+                                            onChange={handleChange}
+                                            variant="outlined"
+                                        />
+                                    </Grid>
+                                    <Grid sx={{ p: 2 }}>
+                                        <TextField
+                                            fullWidth
+                                            label="Sexo"
+                                            name="sexo"
+                                            value={form.sexo}
+                                            onChange={handleChange}
+                                            variant="outlined"
+                                        />
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                            <Grid>
+                                <Grid>
+                                    <Box display="flex" justifyContent="center" alignItems="center" height="100%">
+                                        <img
+                                            src="https://images.dog.ceo/breeds/retriever-golden/n02099601_3007.jpg"
+                                            alt="Pet para adoção"
+                                            style={{ width: '100%', borderRadius: 8 }}
+                                        />
+                                    </Box>
+                                </Grid>
                             </Grid>
                         </Grid>
-                    </Grid>
-                    <Grid>
-                        <Box textAlign="center" mt={2}>
-                            <Button
-                                type="submit"
-                                variant="contained"
-                                sx={{
-                                    backgroundColor: '#FF8C00',
-                                    color: '#FFF',
-                                    px: 4,
-                                    py: 1,
-                                    borderRadius: 2,
-                                    textTransform: 'none'
-                                }}
-                            >
-                                ADOTE-ME
-                            </Button>
-                        </Box>
-                    </Grid>
+                        <Grid>
+                            <Box textAlign="center" mt={2}>
+                                <Button
+                                    type="submit"
+                                    variant="contained"
+                                    sx={{
+                                        backgroundColor: '#FF8C00',
+                                        color: '#FFF',
+                                        px: 4,
+                                        py: 1,
+                                        borderRadius: 2,
+                                        textTransform: 'none'
+                                    }}
+                                >
+                                    ADOTE-ME
+                                </Button>
+                            </Box>
+                        </Grid>
+                    </form>
                 </Paper>
             </Box>
         </div>
