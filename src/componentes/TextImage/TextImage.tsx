91a056e6e1eb4218fc9c9430933d103fdb32cb1b
LuisFelipePeirao <luis.peirao@unifebe.edu.br>
2025-05-15 21:56:23 -0300

    Ajuste de responsividade

--- src/componentes/TextImage/TextImage.tsx
+++ src/componentes/TextImage/TextImage.tsx
@@ -24,19 +24,20 @@ const TextImage: React.FC<TextImageProps> = ({ image, altImg, imgSobreNos, altSo
             <div style={{ padding: "40px 20px", maxWidth: "1100px", margin: "0 auto" }}>
 
                 {/* Bloco 1 */}
-                <div style={{ position: "relative", display: "flex", flexDirection: ImgDireita,  gap: "30px", alignItems: "center", marginBottom: "40px" }}>
+                <div style={{ position: "relative", display: "flex", flexDirection: ImgDireita, gap: "30px", alignItems: "center", marginBottom: "40px", flexWrap: "wrap"}}>
                     <img src={image} alt={altImg} style={{
                         position: "absolute",
                         top: "40px",
                         right: movePatinha,
-                        width: "200px",
+                        width: "150px",
+                        maxWidth: "30%",
                         opacity: opacidadePatinha,
                         zIndex: 0
                     }} />
 
 
-                    <img src={imgSobreNos} alt={altSobreNos} style={{ width: "450px", borderRadius: "10px", boxShadow: "0 0 10px rgba(0,0,0,0.1)" }}/>
-                    <p style={{ textAlign: "justify", lineHeight: "1.6", fontSize: "20px" }}>{children}</p>
+                    <img src={imgSobreNos} alt={altSobreNos} style={{ width: "450px", borderRadius: "10px", maxWidth: "450px", boxShadow: "0 0 10px rgba(0,0,0,0.1)" }}/>
+                    <p style={{ textAlign: "justify", lineHeight: "1.6", fontSize: "18px", flex: "1 1 300px" }}>{children}</p>
                 </div>
             </div>
         </>
